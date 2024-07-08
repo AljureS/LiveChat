a76002e5ed818eccb7deefb1de526b805997ddc1
AljureS <saidsimon2@gmail.com>
2024-07-08 09:43:48 -0500

    Fixed the bug and connect the same HTTP express server to nest and socket

--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,24 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Server } from 'socket.io';
 import { createServer } from 'http';
+import * as express from 'express';
+import { ExpressAdapter } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const expressApp = express(); // Create an Express application
+  const adapter = new ExpressAdapter(expressApp); // So Nest and Socket can use the same http server
+  const app = await NestFactory.create(AppModule, adapter); // Adpater Express to NestJS
 
-  // Obtain de HTTP server from NestJS instance, NEst and Socket use the same HTTP server
-  const httpServer = createServer(app.getHttpServer()); 
-  
-  // Create an instace of Socket.io using the HTTP server //!NestJS
+  const httpServer = createServer(expressApp); // Create HTTP server from Express app
+
+  // Create an instace of Socket.io using the HTTP server 
   const io = new Server(httpServer)
-
+  
   io.on('connection', (socket)=>{
     console.log('user connected', socket.id);
   })
   
+  await app.init();
   await httpServer.listen(3000);
 
   console.log(`Socket.io server is on Port 3000`);
