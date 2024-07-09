22ad5be842fb2042c6e4116564f642915077b099
AljureS <saidsimon2@gmail.com>
2024-07-09 11:59:37 -0500

    Empty porjecto to continue to work in different ways of using sockets in other branches

--- src/main.ts
+++ src/main.ts
@@ -17,9 +17,7 @@ async function bootstrap() {
   
   io.on('connection', (socket)=>{
 
-    socket.on('circlePosition', (position)=>{
-      socket.broadcast.emit('moveCircle', position)
-    })
+    
 
   })
   
--- src/views/js/index.js
+++ src/views/js/index.js
@@ -1,38 +1,3 @@
 const socket = io()
 
-const circle = document.querySelector('#circle')
 
-const darwCircle = (position) => {
-
-    circle.style.top= position.left
-    circle.style.left= position.top
-}
-
-const drag = e => {
-    
-    const clientX = e.clientX
-    const clientY = e.clientY
-
-    const position={
-        top: clientX + 'px', 
-        left: clientY +'px'
-    }
-
-    darwCircle(position)
-    socket.emit('circlePosition', position)
-
-    // circle.style.top= clientY + 'px'
-    // circle.style.left= clientX + 'px'
-}
-
-document.addEventListener('mousedown', e => {
-    document.addEventListener('mousemove', drag)
-})
-
-document.addEventListener('mouseup', e => {
-    document.removeEventListener('mousemove', drag)
-})
-
-socket.on('moveCircle', position => {
-    darwCircle(position)
-})
