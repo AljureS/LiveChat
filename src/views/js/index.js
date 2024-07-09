4efe6fdaa9e52f2fb8ce7a100d2424c33eb7091b
AljureS <saidsimon2@gmail.com>
2024-07-09 10:12:04 -0500

    Basic ON/ONCE/OFF

--- src/views/js/index.js
+++ src/views/js/index.js
@@ -1,9 +1,11 @@
 const socket = io()
 
+//client listener
 socket.on('welcomeMessage', (data) => { 
     text.textContent = data
 })
 
+//client emition
 const emitToServer = document.querySelector('#emitToServer')
 emitToServer.addEventListener('click', () => {
     socket.emit('serverWelcome', 'Hello Server this is a Client')
@@ -12,3 +14,31 @@ emitToServer.addEventListener('click', () => {
 socket.on('everyone', (data) => {
     text2.textContent = data
 })
+
+const emitToLast = document.querySelector('#emitToLast')
+emitToLast.addEventListener('click', () => {
+    socket.emit('last', 'Hello 🫂')
+})
+
+socket.on('salute', (message) => {
+    console.log(message);
+})
+
+//on, once off 
+socket.on("on", ()=>{
+    console.log('event that emits many times, as many as necesary');
+})
+
+socket.once("once", ()=>{
+    console.log('event that emits one times');
+})
+
+const listener =()=>{
+    console.log('event off');
+}
+
+socket.on('off', listener)
+
+setTimeout(()=>{
+    socket.off("off", listener)
+}, 2000)
