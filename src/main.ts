b3ff2bb3206216ab56dfb0779985a6e538eafe1c
AljureS <saidsimon2@gmail.com>
2024-07-09 10:16:54 -0500

    Empty commit before studying event broadcasting

--- src/main.ts
+++ src/main.ts
@@ -14,43 +14,9 @@ async function bootstrap() {
 
   // Create an instace of Socket.io using the HTTP server 
   const io = new Server(httpServer)
-
-  const socketsOnline = [];
   
   io.on('connection', (socket)=>{
 
-    socketsOnline.push(socket.id);
-
-    //Basic Emition from server
-    socket.emit('welcomeMessage', 'Welcome to LiveChat user with ID: ' + socket.id)
-
-    //Basic Listener from server
-    socket.on("serverWelcome", (data)=>{
-      console.log(data);
-    })
-
-    //Total Emition from server to all users
-    io.emit("everyone", socket.id + ' joined the chat')
-
-    //Emision a uno solo 
-    socket.on("last", (message)=>{
-      const lastSocket = socketsOnline[socketsOnline.length - 1];
-      io.to(lastSocket).emit("salute", message); 
-    })
-
-    //? on once off
-    // socket.emit('on', "Holi")
-    // socket.emit('on', "Holi")
-
-    // socket.emit('once', "Holi")
-    // socket.emit('once', "Holi")
-    // socket.emit('once', "Holi")
-
-    socket.emit('off', "Holi")
-
-    setTimeout(()=>{
-      socket.emit('off', "Holi")
-    }, 3000)
   })
   
   await app.init();
