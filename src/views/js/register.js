eb917f394dfd3b7b044229fe5a64a1384b21555b
AljureS <saidsimon2@gmail.com>
2024-07-19 13:36:39 -0500

    Found bug tryng to use admin ui

--- src/views/js/register.js
+++ src/views/js/register.js
@@ -1,1 +1,11 @@
-const button = docuemnt.querySelector('#login');
+const login = document.querySelector('#login');
+
+login.addEventListener('click', () => {
+    const user = document.querySelector('#username').value;
+
+    if (user != "") {
+        document.cookie = `username=${user}`
+        document.location.href="../"
+    } else alert ('Please, write your username')
+    
+})
